8be088f47e3246c16fdae77143f26c7fdab46360
Adarsh Kumar <90547844+whoadarshkumar@users.noreply.github.com>
2022-04-15 10:52:19 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+var randomNumber1 = Math.random();
+randomNumber1=randomNumber1*6;
+randomNumber1=Math.floor(randomNumber1)+1;
+
+var randomDiceImage = "dice" + randomNumber1 + ".png";
+
+var randonImageSource = "images/" + randomDiceImage;
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randonImageSource);
+
+var randomNumber2 = Math.random();
+randomNumber2=randomNumber2*6;
+randomNumber2=Math.floor(randomNumber2)+1;
+
+var randomDiceImage2 = "dice" + randomNumber2 + ".png";
+
+var randonImageSource2 = "images/" + randomDiceImage2;
+
+var image2 = document.querySelectorAll("img")[1];
+
+image2.setAttribute("src", randonImageSource2);
+
+if(randomNumber1 > randomNumber2){
+  document.querySelector("h1").innerHTML = "Player 1 wins";
+}
+else if(randomNumber2 > randomNumber1){
+  document.querySelector("h1").innerHTML = "Player 2 wins";
+}
+else if(randomNumber1 = randomNumber2){
+  document.querySelector("h1").innerHTML = "Draw!";
+}
