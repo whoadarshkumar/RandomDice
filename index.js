d2592c00512b95950cc78d360786ace91e303252
Adarsh Kumar <90547844+whoadarshkumar@users.noreply.github.com>
2022-04-15 11:05:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,11 +4,10 @@ randomNumber1=Math.floor(randomNumber1)+1;
 
 var randomDiceImage = "dice" + randomNumber1 + ".png";
 
-var randonImageSource = randomDiceImage;
 
 var image1 = document.querySelectorAll("img")[0];
 
-image1.setAttribute("src", randonImageSource);
+image1.setAttribute("src", randomDiceImage);
 
 var randomNumber2 = Math.random();
 randomNumber2=randomNumber2*6;
@@ -16,11 +15,9 @@ randomNumber2=Math.floor(randomNumber2)+1;
 
 var randomDiceImage2 = "dice" + randomNumber2 + ".png";
 
-var randonImageSource2 = randomDiceImage2;
-
 var image2 = document.querySelectorAll("img")[1];
 
-image2.setAttribute("src", randonImageSource2);
+image2.setAttribute("src", randomDiceImage2);
 
 if(randomNumber1 > randomNumber2){
   document.querySelector("h1").innerHTML = "Player 1 wins";
